3c63d9d91f859412345e7e8fe38c7635e212367d
EviLAsRz <alvarosantosrom9@gmail.com>
2023-05-09 12:15:27 +0200

    prototipo del HUD listo

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -8,8 +8,8 @@ public class HudManager : MonoBehaviour
 
 {
     public TMP_Text time;
-    public TMP_Text month;
-    public TMP_Text year;
+    //public TMP_Text month; Por si se quiere cambiar el texto de abajo, aunque en principio
+    //public TMP_Text year;  cada mapa tiene su timestamp.
     float gameTime = 0.0f;
 
     void Update()
@@ -18,6 +18,6 @@ public class HudManager : MonoBehaviour
         int hours = Mathf.FloorToInt(gameTime / 3600);  //horas
         int minutes = Mathf.FloorToInt(gameTime / 60);  //minutos
         int seconds = Mathf.FloorToInt(gameTime % 60);  //segundos
-        time.text = "PM  " + string.Format("{0:0}:{1:00}:{2:00}",hours, minutes, seconds);
+        time.text = "PM  " + string.Format("{0:0}:{1:00}:{2:00}",hours, minutes, seconds); //formato 0:00:00
     }
 }
