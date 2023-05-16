1760d16b46e85ce744c3ae6af63495fe3eb14240
EviLAsRz <alvarosantosrom9@gmail.com>
2023-05-16 11:02:02 +0200

    fade inicial añadido, a mirar si se puede añadir sonido de inicio

---
+++ Assets/Scripts/StartingScreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class StartingScreen : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject startingScreen;
+
+    public float waitTime;
+
+
+    void Start()
+    {
+        startingScreen.SetActive(true);
+        player = GameObject.FindWithTag("Player");
+        player.GetComponent<FirstPersonController>().enabled = false;
+
+        StartCoroutine(Starting());
+    }
+
+    IEnumerator Starting()
+    {
+        yield return new WaitForSeconds(waitTime);
+        startingScreen.SetActive(false);
+        player.GetComponent<FirstPersonController>().enabled = true;
+
+    }
+
+
+
+
+    void Update()
+    {
+        
+    }
+}
